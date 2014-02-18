162ed0f6a1781cd054dd0d482f6db29cc24a353e
Stephen Babineau <tien_ralter@hotmail.com>
2014-02-17 19:32:52 -0800

    Add docstring

--- mysocket.py
+++ mysocket.py
@@ -1,17 +1,21 @@
 #! usr/bin/env python
 
 import socket
+"""Simple echo server with no arguments"""
 
 
 def run_server():
+
     server_socket = socket.socket(socket.AF_INET,
                                   socket.SOCK_STREAM,
                                   socket.IPPROTO_IP)
     server_socket.bind(("127.0.0.1", 50000))
     server_socket.listen(1)
-    while True:
+
+    while True:  # endless loop to permit server to continue echo function
         conn, addr = server_socket.accept()
         message = conn.recv(1024)
         conn.sendall(message)
         conn.close()
+
     server_socket.close()
